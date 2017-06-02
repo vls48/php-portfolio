90ef2daad351de06ad36b681fdaa2d3d8804215f
vls48 <vls48@drexel.edu>
2017-06-02 18:39:14 -0400

    updates based on feedback
    added button, fixed spacing, rearranged content so it reads better,
    worked on consistency

--- source/main.js
+++ source/main.js
@@ -2,12 +2,17 @@
 var iconObj = document.querySelector('.ham-menu');
 var menulist = document.getElementById('nav-list');
 var rowNav = document.querySelector('.row-nav');
+var w = window.innerWidth;
+var h = window.innerHeight;
 
 //LISTENER
 iconObj.addEventListener('click', function(evt) {
 	menu(evt, '.ham-menu');
 }, false);
 
+window.addEventListener("resize", function(evt) {
+	bringback(evt, '.ham-menu');
+}, false);
 
 //FUNCTION
 function menu(evt) {
@@ -15,4 +20,11 @@ function menu(evt) {
 	menulist.classList.toggle('visually-hidden');
 	rowNav.classList.toggle('full-length');
 	rowNav.classList.toggle('row-nav');
+}
+
+function bringback(evt) {
+		console.log('hi');
+
+
+
 }
